ec2e9293dcbfff49c33018b48c1d6aed3dfed49d
Kerim Can Mersin <73068663+kcanmersin@users.noreply.github.com>
2023-10-19 00:26:26 +0300

    .

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-rooms = { 
+rooms = [
     {"id":1, "name":"lets learn pyhton"},
     {"id":2, "name":"design with me"},
     {"id":3, "name":"front end dev "},
-}
+]
 def home(request):
-    return render(request,"home.html", {"rooms":rooms})
+    context={"rooms":rooms}
+    return render(request,"home.html", context)
 def room(request):
     return render(request,"room.html")
